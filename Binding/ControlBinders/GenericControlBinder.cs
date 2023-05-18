7e63ae91813d48ee02e84af1064edc19d92adde2
Dangles <aidand91@gmail.com>
2023-05-19 02:16:17 +1000

    Implement one-way scene list binding

--- Binding/ControlBinders/GenericControlBinder.cs
+++ Binding/ControlBinders/GenericControlBinder.cs
@@ -2,6 +2,7 @@ using Godot.Community.ControlBinding.EventArgs;
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Godot.Community.ControlBinding.ControlBinders;
 
@@ -33,7 +34,7 @@ public partial class GenericControlBinder : ControlBinderBase
 
     public override void OnListItemChanged(object entry)
     {
-        throw new NotImplementedException();
+        // do nothing
     }
 
     public override void OnObservableListChanged(ObservableListChangedEventArgs eventArgs)
@@ -47,27 +48,40 @@ public partial class GenericControlBinder : ControlBinderBase
         if(_boundControl == null)
             _boundControl = _bindingConfiguration.BoundControl.Target as Godot.Control;
 
-        // list item references are cached against a node ID so they can be removed from the
-        // list of children when removed from the backing ObservableList 
+        
         if(eventArgs.ChangeType == ObservableListChangeType.Add)
         {
             foreach(var addition in eventArgs.ChangedEntries)
             {
                 var sceneItem = _bindingConfiguration.SceneFormatter.Format(addition);                
                 _boundControl.AddChild(sceneItem);
+
+                // list item references are cached against a node ID so they can be removed from the
+                // list of children when removed from the backing ObservableList 
                 _controlChildCache.Add(addition, sceneItem.GetInstanceId());
             }
-            
         }
 
         if(eventArgs.ChangeType == ObservableListChangeType.Remove)
         {
-            
+            foreach(var removedItem in eventArgs.ChangedEntries)
+            {
+                // get the corresponding scene item
+                var instanceId =_controlChildCache.GetValueOrDefault(removedItem);
+                var sceneItem = _boundControl.GetChildren().FirstOrDefault(x => x.GetInstanceId() == instanceId);
+                if(sceneItem != null)
+                {
+                    _boundControl.RemoveChild(sceneItem);
+                }
+            }
         }
 
         if(eventArgs.ChangeType == ObservableListChangeType.Clear)
         {
-            
+            foreach(var child in _boundControl?.GetChildren())
+            {
+                _boundControl.RemoveChild(child);
+            }
         }
     }
 }
