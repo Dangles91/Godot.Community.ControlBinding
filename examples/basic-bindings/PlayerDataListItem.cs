3028c5f7f0f0ec4f3845a646a61831189053df0d
Dangles <aidand91@gmail.com>
2023-05-20 15:12:49 +1000

    Add TwoWay binding support to example project

--- examples/basic-bindings/PlayerDataListItem.cs
+++ examples/basic-bindings/PlayerDataListItem.cs
@@ -1,28 +1,31 @@
 using ControlBinding;
 using Godot;
 using Godot.Community.ControlBinding;
-using System;
 
 public partial class PlayerDataListItem : ObservableNode
 {
 
-	private PlayerData ViewModelData { get; set; }
+    private PlayerData ViewModelData { get; set; }
 
     public override void SetViewModelData(object viewModelData)
     {
-		ViewModelData = viewModelData as PlayerData;
+        ViewModelData = viewModelData as PlayerData;
         base.SetViewModelData(viewModelData);
-    }	
+    }
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		BindProperty("%TextEdit", "Text", "ViewModelData.Health", BindingMode.TwoWay);
-		base._Ready();
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        BindProperty("%TextEdit", "Text", "ViewModelData.Health", BindingMode.TwoWay);
+        GetNode<Button>("%Button").Pressed += () =>
+        {
+            this.QueueFree();
+        };
+        base._Ready();
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
