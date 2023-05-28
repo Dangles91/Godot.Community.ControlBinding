869908862719a9b19e72e259ca0dd0fd176a7516
Dangles <aidand91@gmail.com>
2023-05-28 22:21:20 +1000

    Add basic custom control example
    Requires tidy up before being merged

---
+++ examples/basic-bindings/CustomControl.cs
@@ -0,0 +1,34 @@
+using Godot;
+using Godot.Community.ControlBinding;
+using Godot.Community.ControlBinding.Extensions;
+using Godot.Community.ControlBinding.Formatters;
+using Godot.Community.ControlBinding.Interfaces;
+using PropertyChanged.SourceGenerator;
+using System;
+
+namespace ControlBinding;
+
+public partial class CustomControl : Control, IObservableObject
+{
+
+    [Notify]
+    private string _input;
+
+    private BindingContext _bindingContext;
+
+    public override void _Ready()
+    {
+        _bindingContext = new BindingContext(this);
+
+        GetNode<LineEdit>("%LineEdit").BindProperty(_bindingContext, nameof(LineEdit.Text), nameof(Input), BindingMode.OneWayToTarget,
+            new ValueFormatter
+            {
+                FormatTarget = (v, _) => $"Input: {v}",
+            });
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+}
